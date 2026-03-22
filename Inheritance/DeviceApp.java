3b211e033fde648af4f161f881abb84bf2e40379
sonamtripathi625-commits <sonamtripathi625@gmail.com>
2026-03-22 19:53:09 +0530

    [Sonam] Inheritance added

---
+++ Inheritance/DeviceApp.java
@@ -0,0 +1,38 @@
+package com.gla.Inheritance;
+
+public class DeviceApp {
+
+    static class Device {
+        String deviceId;
+        String status;
+
+        Device(String id, String status) {
+            this.deviceId = id;
+            this.status = status;
+        }
+
+        void displayStatus() {
+            System.out.println("Device ID: " + deviceId + ", Status: " + status);
+        }
+    }
+
+    static class Thermostat extends Device {
+        int temperatureSetting;
+
+        Thermostat(String id, String status, int temp) {
+            super(id, status);
+            this.temperatureSetting = temp;
+        }
+
+        @Override
+        void displayStatus() {
+            super.displayStatus();
+            System.out.println("Temperature: " + temperatureSetting);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thermostat t = new Thermostat("T101", "ON", 25);
+        t.displayStatus();
+    }
+}
